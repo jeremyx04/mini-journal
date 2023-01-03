2e4b6317cc5ea7fa14f11a8ce923b217d499e585
Jeremy Xue <jer1506xue@gmail.com>
2023-01-02 22:47:20 -0500

    add .env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require('pg').Pool;
+require('dotenv').config({path:'../.env'});
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'abcd246',
+    password: process.env.PW,
     host: 'localhost',
     port: 5432,
     database: 'entries'
--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const app = express();
 const cors = require('cors'); 
 const pool = require('./db');
 
+
 app.use(cors());
 app.use(express.json());
 
 app.post('/entrys', async(req, res) => {
     try{
-        
+        const { liked, disliked } = req.body;
+        const newEntry = await pool.query('INSERT INTO entry_table (liked) VALUES($1)',[liked]);
+        console.log(disliked);
+        res.json(newEntry);
     } catch (err){
         console.log(err.message);
     }
