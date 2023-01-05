c0747c634236e5b6488f304ec2865f1c1a2bdd8e
Jeremy Xue <jer1506xue@gmail.com>
2023-01-04 19:32:27 -0500

    header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Header.css'
+export default function Header(){
+    const current = new Date();
+    let date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+        ];
+    const parse = (date) => {
+        let arr = date.split('/');
+        return {    
+            day: arr[0],
+            month: months[arr[1]],
+            year: arr[2]
+        };
+    };
+    return(
+        <div className='titlebox'>
+            <div className='title'>
+                {parse(date).month + " " + parse(date).day + " " + parse(date).year}
+            </div>
+        </div>
+    )
+}
